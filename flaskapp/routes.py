64dd8241b7a201357308bea6a14e320e8360410c
Jakob Nacanaynay <jnac8080@gmail.com>
2022-04-20 12:55:16 -0700

    Make it look decent

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -4,12 +4,19 @@ from flask import url_for, render_template, request
 
 @app.route('/')
 @app.route('/home')
-def index():
+def home():
     return render_template('home.html', title='Home')
 
 @app.route('/entry', methods=['GET', 'POST'])
 def entry():
     form = EntryForm()
+    if form.validate_on_submit():
+        print(form.data)
+    return render_template('entry.html', title='Entry', form=form)
+
+@app.route('/analysis', methods=['GET', 'POST'])
+def analysis():
+    form = CTFDLoginForm()
     if form.validate_on_submit():
         print(form.data)
     return render_template('entry.html', title='Entry', form=form)
