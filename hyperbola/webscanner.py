2ff07aa8abe14714a59b1487a69ddbf5583a4d5d
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-04-20 07:15:28 -0700

    Update webscanner.py

--- hyperbola/webscanner.py
+++ hyperbola/webscanner.py
@@ -1,8 +1,8 @@
 import requests
 import re
-site = 'http://texasholdem-v2.heyguy__.repl.co/play.1'
-base = '/'.join(site.split('/')[0:3])
-flag = r''
-print(base)
+site = 'http://texasholdem-v2.heyguy__.repl.co/play.a'
+base = site.split('/')
+if base[0] in ('http:','https:'): base = '/'.join(base[0:3])
+flag = r'flag\{.*?\}?'
 r = requests.get(site)
 print(r.text)
