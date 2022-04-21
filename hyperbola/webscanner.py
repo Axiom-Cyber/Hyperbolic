411b6acff42df9b765e30984fffbf7357d46fc2b
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-04-21 12:55:54 -0700

    Update webscanner.py

--- hyperbola/webscanner.py
+++ hyperbola/webscanner.py
@@ -1,12 +1,37 @@
 import requests
 import re
-site = 'http://finally-make-a-decision-inator.heyguy__.repl.co/decide'
-base = re.search(r'^(https?://)?.*?(?=/)', site).group(0)
-flag = r'flag\{[^ ]*?\}'
-r = requests.get(site)
-for i in re.findall(flag, r.text): print('flag found:', i)
-for x,z,h in re.findall(r'(href|src)\s*?=\s*?(["\'])(.*?)\2', r.text): 
-    if h[0]=='/': nurl = base + h
-    else: nurl = base + '/' + h
-    r = requests.get(nurl)
+def scan(site, flag=r'flag\{\S*?\}'):
+    site = 'https://jupiter.challenges.picoctf.org/problem/36474'
+    base = site.replace(r'((?:https?:\/\/)?.*?)\/.*', '\1')
+    base = "https://jupiter.challenges.picoctf.org/problem/36474"
+    scan_file(flag, site)
+    scan_robots(flag, base)
+        
+def scan_file(flag, url, rec=0):
+    r = requests.get(url)
+    for i in r.cookies:
+        for j in re.findall(flag, i.value): print('flag found:', j)
+        for j in re.findall(flag, i.name): print('flag found:', j)
     for i in re.findall(flag, r.text): print('flag found:', i)
+    for i in re.findall(r'\/\*.*?\*\/', r.text): print('comment:', i)
+    for i in re.findall(r'\/\/.*?[\n$]', r.text): print('comment:', i)
+    for i in re.findall(r'((?:https?:\/\/)?[^\s/]*?\.[^\s/]*?)\/\S*', r.text): print('link:', i)
+    for i in re.findall(r'((?:https?:\/\/)?[^\s/]*?\.[^\s/]*?)\/\S*', r.text): print('link:', i)
+    base = url.replace(r'((?:https?:\/\/)?.*?)\/.*', '\1')
+    if rec<5:
+        for x,z,h in re.findall(r'(href|src)\s*?=\s*?(["\'])(.*?)\2', r.text): 
+            if h[0]=='/': nurl = base + h
+            else: nurl = base + '/' + h
+            scan_file(flag, nurl, rec+1)
+
+def scan_robots(flag, base):
+    url = base+'/robots.txt'
+    r = requests.get(url)
+    for i in r.cookies:
+        for j in re.findall(flag, i.value): print('flag found:', j)
+        for j in re.findall(flag, i.name): print('flag found:', j)
+    for i in re.findall(flag, r.text): print('flag found:', i)
+    for i in re.findall(r'(?<=[aA]llow: )(\/.*?)$', r.text, flags = re.MULTILINE): 
+        scan_file(flag, base+i)
+
+scan('',r'picoCTF\{\S\}')
