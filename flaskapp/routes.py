3a95b6c48d4675c36990c42a260800e7de38cc3f
Jakob Nacanaynay <jnac8080@gmail.com>
2022-05-16 12:54:49 -0700

    Testing spa stuff

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,26 +1,30 @@
-from flaskapp import app
+from flaskapp import app, csrf
 from flaskapp.forms import CTFDLoginForm, EntryForm
-from flask import url_for, render_template, request
+from flask import url_for, render_template, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('home.html', title='Home')
+values = {}
 
-@app.route('/entry', methods=['GET', 'POST'])
-def entry():
-    form = EntryForm()
-    if form.validate_on_submit():
-        filename = secure_filename(form.file.data.filename)
-        form.file.data.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        print(form.data)
-    return render_template('entry.html', title='Entry', form=form)
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def home(path):
+    return render_template('home.html', **values)
 
-@app.route('/ctfd-scan', methods=['GET', 'POST'])
-def ctfd():
-    form = CTFDLoginForm()
-    if form.validate_on_submit():
-        print(form.data)
-    return render_template('ctfd.html', title='CTFd Scan', form=form)
+@app.route("/heartbeat")
+def heartbeat():
+    return jsonify({"status": "healthy"})
+
+@app.route("/hello", methods=["POST"])
+@csrf.exempt
+def hello():
+    global values
+    values['message'] = 'hello'
+    return 'hello'
+
+@app.route("/goodbye", methods=["POST"])
+@csrf.exempt
+def goodbye():
+    global values
+    values['goodbye'] = 'goodbye'
+    return 'goodbye'
