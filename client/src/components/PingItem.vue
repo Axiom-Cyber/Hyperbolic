8bb570b3023e56dd9f3a5d5eb780c1556a120117
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2022-05-24 20:08:49 -0700

    Working Vue with ping

--- client/src/components/PingItem.vue
+++ client/src/components/PingItem.vue
@@ -1,34 +1,16 @@
 <template>
-    <div>
-        <p>{{ msg }}</p>
-    </div>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
   name: 'PingItem',
   data() {
     return {
-      msg: '',
+      msg: 'Hello!',
     };
-  },
-  methods: {
-    getMessage() {
-      const path = 'http://localhost:5000/ping';
-      axios.get(path)
-        .then((res) => {
-          this.msg = res.data;
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.error(error);
-        });
-    },
-  },
-  created() {
-    this.getMessage();
   },
 };
 </script>
