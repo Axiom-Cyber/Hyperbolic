42db2f4aae21fbdc7bdc12e4bf6580da1a8e5b6f
Jakob Nacanaynay <jnac8080@gmail.com>
2022-05-27 06:52:26 -0700

    Delete client

--- client/src/main.js
+++
@@ -1,7 +0,0 @@
-// npm run serve
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import 'bootstrap/dist/css/bootstrap.css';
-
-createApp(App).use(router).mount('#app');
