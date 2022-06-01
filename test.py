242d519996b764a75bbf4aa22400595244cc66d3
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-06-01 12:34:21 -0700

    init

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-from hyperbola.connection import Worker
-
-w = Worker('https://demo.ctfd.io', 'user', 'password')
-print(w)
-print(repr(w))
-w.close()
+from hyperbola.defaults import Logger, Commander
