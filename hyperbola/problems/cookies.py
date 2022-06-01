f220bea11c2f3f2729f63af382555d942e114a37
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-06-01 12:55:44 -0700

    cookie worker

--- hyperbola/problems/cookies.py
+++ hyperbola/problems/cookies.py
@@ -4,5 +4,17 @@ defaults.Commander.add_worker('url')
 class Cookie:
     async def return_solution(self, data):
         post = requests.post(data)
+        newdata = []
         try:
             get = requests.get(data)
+            for c in get.cookies:
+                newdata.append(c.name)
+                newdata.append(c.name)
+        except: pass
+        try:
+            post = requests.post(data)
+            for c in post.cookies:
+                newdata.append(c.name)
+                newdata.append(c.name)
+        except: pass
+        return {'logs':[], 'newdata':[{'type':'text', 'data':i} for i in newdata]}
