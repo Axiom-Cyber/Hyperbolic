2b8c26778d2a3c23406126feb256931fdebcbfb7
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-06-07 09:34:40 -0700

    format new solvers

--- hyperbola/problems/guess-extension.py
+++ hyperbola/problems/guess-extension.py
@@ -1,11 +1,10 @@
 from filetype import guess
 import defaults
 
-def guessExtension(filePath):
-    try:
-        print("Filetype: " + str(guess(filePath).extension))
-    except:
-        print("Couldn't find filetype")
-
-if __name__ == "__main__":
-    guessExtension()
+defaults.Commander.add_worker('filepath')
+class Extension:
+    def guessExtension(filePath):
+        try:
+            print("Filetype: " + str(guess(filePath).extension))
+        except:
+            print("Couldn't find filetype")
--- hyperbola/problems/render-binary.py
+++ hyperbola/problems/render-binary.py
@@ -4,36 +4,35 @@ from PIL import Image
 import numpy as np
 import defaults
 
-def renderImage(pathName, outName=None, width="guess"):
-    if outName == None:
-        outName = pathName + "out"
-    outName = outName.replace(".png", "")
-    file=open(pathName, "rb")
-    size = (path.getsize(pathName) / 4) - 4
+defaults.Commander.add_worker('filepath')
+class RenderBinary:
+    def return_solution(pathName, outName=None, width="guess"):
+        if outName == None:
+            outName = pathName + "out"
+        outName = outName.replace(".png", "")
+        file=open(pathName, "rb")
+        size = (path.getsize(pathName) / 4) - 4
 
-    if width == "guess":
-        widths = []
-        for width in range(50, int(size)-50):
-            if (size/width) % 1 == 0 and size/(width**2) > 0.25 and size/(width**2) < 4:
-                widths.append(width)
+        if width == "guess":
+            widths = []
+            for width in range(50, int(size)-50):
+                if (size/width) % 1 == 0 and size/(width**2) > 0.25 and size/(width**2) < 4:
+                    widths.append(width)
 
-    else:
-        widths = [int(width)]
-    
-    for width in widths:
-        file.seek(0)
-        pixels = []
-        for i in range(int(size/width)):
-            row = []
-            for j in range(width):
-                color = file.read(4)
-                row.append(tuple(int(k) for k in color))
-            pixels.append(row)
+        else:
+            widths = [int(width)]
         
-        outArray = np.array(pixels, dtype=np.uint8)
-        Image.fromarray(outArray).save(outName+str(width)+".png")
+        for width in widths:
+            file.seek(0)
+            pixels = []
+            for i in range(int(size/width)):
+                row = []
+                for j in range(width):
+                    color = file.read(4)
+                    row.append(tuple(int(k) for k in color))
+                pixels.append(row)
+            
+            outArray = np.array(pixels, dtype=np.uint8)
+            Image.fromarray(outArray).save(outName+str(width)+".png")
 
-    file.close()
-
-if __name__ == "__main__":
-    renderImage("C:\\Users\\clemevin000\\Downloads\\flag.data", "C:\\Users\\clemevin000\\Downloads\\out.png", "guess")
+        file.close()
