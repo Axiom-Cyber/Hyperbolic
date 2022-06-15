c132ef8d2a2b34e24a0b1f8f8a28ef7ce66bff69
Vincent Clement <vincent.jude.clement@gmail.com>
2022-06-15 09:18:51 -0700

    Finished Decompression

--- hyperbola/problems/decompress.py
+++ hyperbola/problems/decompress.py
@@ -2,15 +2,13 @@ from filetype import guess
 import zipfile
 from os import walk
 
-#@defaults.Commander.add_worker('filepath')
-#class Decompress:
-def decompress(filepath, out):
-    if guess(filepath) != None and guess(filepath).extension == "zip":
-        with zipfile.ZipFile(filepath, 'r') as zip_ref:
-            zip_ref.extractall(out)
-            for (dirpath, dirnames, filenames) in walk(out):
-                for file in filenames:
-                    decompress(out + "/" + file, out + "/" + file[::-4])
-                break
-
-decompress(input(), "C:/Users/clemevin000/Desktop/out")
+@defaults.Commander.add_worker('filepath')
+class Decompress:
+    def decompress(filepath):
+        if guess(filepath) != None and guess(filepath).extension == "zip":
+            with zipfile.ZipFile(filepath, 'r') as zip_ref:
+                zip_ref.extractall(filepath[:-4])
+                for (dirpath, dirnames, filenames) in walk(filepath[:-4]):
+                    for file in filenames:
+                        decompress(filepath[:-4] + "/" + file)
+                    break
