b9eb3a0ffd2d862cbda29b83bafb4a87274a75f8
John Jorgensen <63758468+johnjohntheleprechaun@users.noreply.github.com>
2022-06-22 18:25:55 -0700

    encoder

--- hyperbola/problems/base_conversion.py
+++ hyperbola/problems/base_conversion.py
@@ -12,3 +12,14 @@ class BaseDecoder:
         out = f"base16: {b16}; base32: {b32}; base64: {b64}"
 
         return {"logs": [], "newdata": [{"type": "text", "data": out}], "end": False}
+
+@hyperbola.Commander.add_worker("text")
+class BaseEncoder:
+    def return_solution(text: str): # string formatted "[base][e/d] [text]" e=encode d=decode
+        b16 = base64.b16encode(text.encode()).decode()
+        b32 = base64.b32encode(text.encode()).decode()
+        b64 = base64.b64encode(text.encode()).decode()
+
+        out = f"base16: {b16}; base32: {b32}; base64: {b64}"
+
+        return {"logs": [], "newdata": [{"type": "text", "data": out}], "end": False}
