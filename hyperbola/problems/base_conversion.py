ff67aa6ed2d21b14c69689310aad2efb0dea1d26
John Jorgensen <63758468+johnjohntheleprechaun@users.noreply.github.com>
2022-06-22 18:28:30 -0700

    remove outdated comments

--- hyperbola/problems/base_conversion.py
+++ hyperbola/problems/base_conversion.py
@@ -1,10 +1,9 @@
-import re
 import base64
 import hyperbola
 
 @hyperbola.Commander.add_worker("text")
 class BaseDecoder:
-    def return_solution(text: str): # string formatted "[base][e/d] [text]" e=encode d=decode
+    def return_solution(text: str):
         b16 = base64.b16decode(text.encode()).decode()
         b32 = base64.b32decode(text.encode()).decode()
         b64 = base64.b64decode(text.encode()).decode()
@@ -15,7 +14,7 @@ class BaseDecoder:
 
 @hyperbola.Commander.add_worker("text")
 class BaseEncoder:
-    def return_solution(text: str): # string formatted "[base][e/d] [text]" e=encode d=decode
+    def return_solution(text: str):
         b16 = base64.b16encode(text.encode()).decode()
         b32 = base64.b32encode(text.encode()).decode()
         b64 = base64.b64encode(text.encode()).decode()
