c42c037c2930a5840749ca23cdc05fed2cc7f8f9
Vincent Clement <vincent.jude.clement@gmail.com>
2022-06-26 10:31:55 -0500

    Fixed images and render binary

--- hyperbola/problems/render-binary.py
+++ hyperbola/problems/render-binary.py
@@ -2,38 +2,36 @@ import matplotlib.pyplot as plt
 from os import path
 from PIL import Image
 import numpy as np
+import re
 import hyperbola
+import filetype
 
 @hyperbola.Commander.add_worker('filepath')
 class RenderBinary:
     def return_solution(self, pathName, outName=None, width="guess"):
-        if outName == None:
-            outName = pathName + "out"
-        outName = outName.replace(".png", "")
-        file=open(pathName, "rb")
-        size = (path.getsize(pathName) / 4) - 4
-
-        if width == "guess":
+        if (filetype.guess(pathName) and filetype.guess(pathName).extension in ['jpeg', 'gif', 'png', 'apng', 'svg', 'bmp']):
+            if outName == None:
+                outName = pathName + "out"
+            outName = re.sub(r"\.[^.]*$", "", pathName)
+            file=open(pathName, "rb")
+            size = (path.getsize(pathName) / 4) - 4
             widths = []
             for width in range(50, int(size)-50):
                 if (size/width) % 1 == 0 and size/(width**2) > 0.25 and size/(width**2) < 4:
                     widths.append(width)
-
-        else:
-            widths = [int(width)]
-        
-        for width in widths:
-            file.seek(0)
-            pixels = []
-            for i in range(int(size/width)):
-                row = []
-                for j in range(width):
-                    color = file.read(4)
-                    row.append(tuple(int(k) for k in color))
-                pixels.append(row)
             
-            outArray = np.array(pixels, dtype=np.uint8)
-            Image.fromarray(outArray).save(outName+"/"+str(width)+".png")
+            for width in widths:
+                file.seek(0)
+                pixels = []
+                for i in range(int(size/width)):
+                    row = []
+                    for j in range(width):
+                        color = file.read(4)
+                        row.append(tuple(int(k) for k in color))
+                    pixels.append(row)
+                
+                outArray = np.array(pixels, dtype=np.uint8)
+                Image.fromarray(outArray).save(outName + "/" + str(width) + ".png")
 
-        file.close()
-        return {'logs':[],'newdata':[{"type":"image", "data":outName}],'end':False}
+            file.close()
+            return {'logs':[],'newdata':[{"type":"image", "data":outName}],'end':False}
