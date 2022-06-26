9272a13451d40092c412761f597c70063a1242d3
Asher-W <85578685+Asher-W@users.noreply.github.com>
2022-06-26 06:53:56 -0700

    update base conversion

--- hyperbola/problems/base_conversion.py
+++ hyperbola/problems/base_conversion.py
@@ -1,22 +1,22 @@
-# John - we need to talk about some fixes. We can discuss them in class tommorow. This is commented out for testing purposes now.
-# import base64
-# import hyperbola
-# import re
+import base64
+import hyperbola
+import re
 
-# @hyperbola.Commander.add_worker("text")
-# class BaseDecoder:
-#     async def return_solution(self, text: str):
-#         b16 = base64.b16decode(text.encode()).decode()
-#         b32 = base64.b32decode(text.encode()).decode()
-#         b64 = base64.b64decode(text.encode()).decode()
+@hyperbola.Commander.add_worker("text")
+class BaseDecoder:
+    async def return_solution(self, text: str):
 
-#         return {"logs": [], "newdata": [{"type": "text", "data": i} for i in [b16,32,64]], "end": False}
+        b16 = [base64.b16decode(i.encode()).decode() for i in re.findall(r'[0-9A-F]+')]
+        b32 = [base64.b32decode(text.encode()).decode() for i in re.findall(r'[0-9A-V]+')]
+        b64 = [base64.b64decode(text.encode()).decode() for i in re.findall(r'[0-9A-Za-z]+')]
 
-# @hyperbola.Commander.add_worker("text")
-# class BaseEncoder:
-#     async def return_solution(self, text):
-#         b16 = base64.b16encode(text.encode()).decode()
-#         b32 = base64.b32encode(text.encode()).decode()
-#         b64 = base64.b64encode(text.encode()).decode()
+        return {"logs": [], "newdata": [{"type": "text", "data": i} for i in b16+b32+b64], "end": False}
 
-#         return {"logs": [], "newdata": [{"type": "text", "data": i} for i in [b16, b32, b64]], "end": False}
+@hyperbola.Commander.add_worker("text")
+class BaseEncoder:
+    async def return_solution(self, text):
+        b16 = [base64.b16encode(i.encode()).decode() for i in re.findall(r'[0-9A-F]+')]
+        b32 = [base64.b32encode(text.encode()).decode() for i in re.findall(r'[0-9A-V]+')]
+        b64 = [base64.b64encode(text.encode()).decode() for i in re.findall(r'[0-9A-Za-z]+')]
+
+        return {"logs": [], "newdata": [{"type": "text", "data": i} for i in b16+b32+b64], "end": False}
