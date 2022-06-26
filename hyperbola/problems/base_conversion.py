74a009333101315ddfa499407053b7a8c7a94b12
John Jorgensen <63758468+johnjohntheleprechaun@users.noreply.github.com>
2022-06-26 07:38:19 -0700

    try except in base decoder

--- hyperbola/problems/base_conversion.py
+++ hyperbola/problems/base_conversion.py
@@ -5,10 +5,15 @@ import re
 @hyperbola.Commander.add_worker("text")
 class BaseDecoder:
     async def return_solution(self, text: str):
-        b2 = [chr(int(i,2)) for i in re.findall(r'[01]+')]
-        b16 = [base64.b16decode(i.encode()).decode() for i in re.findall(r'[0-9A-F]+')]
-        b32 = [base64.b32decode(i.encode()).decode() for i in re.findall(r'[0-9A-V]+')]
-        b64 = [base64.b64decode(i.encode()).decode() for i in re.findall(r'[0-9A-Za-z]+')]
+        #try to decode in each base, but if it fails, return an empty list
+        try: b2 = [chr(int(i,2)) for i in re.findall(r'[01]+')]
+        except: b2 = []
+        try: b16 = [base64.b16decode(i.encode()).decode() for i in re.findall(r'[0-9A-F]+')]
+        except: b16 = []
+        try: b32 = [base64.b32decode(i.encode()).decode() for i in re.findall(r'[0-9A-V]+')]
+        except: b32 = []
+        try: b64 = [base64.b64decode(i.encode()).decode() for i in re.findall(r'[0-9A-Za-z]+')]
+        except: b64 = []
         
         return {"logs": [], "newdata": [{"type": "number", "data": i} for i in b2+b16+b32+b64], "end": False}
 
