d595aa2ef336823533b26104b9d7649069d5c52a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 08:22:51 +0000

    Adding tarfile member sanitization to extractall()

--- hyperbola/problems/decompress.py
+++ hyperbola/problems/decompress.py
@@ -40,7 +40,26 @@ class Decompress:
             elif extension == "tar":
                 with tarfile.open(filepath, "r") as tar:
                     compressed = True
-                    tar.extractall(re.sub(r"\/[^\/]*$", "", filepath))
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, re.sub("\/[^\/]*$","",filepath))
                     logs.append({"type": "text", "msg": "Untarred"})
             else:
                 return {"logs": [{"type": "download", "msg": filepath}], "newdata": [{"type": "filepath", "data": filepath}]}
